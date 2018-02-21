fd8b7e6b3dd2192d989c0c36fa6463813b3ede2a
Connor McArthur <cmcarthur@rjmetrics.com>
2018-02-20 23:47:53 -0500

    word count

---
+++ models/moby_dick.sql
@@ -0,0 +1,5 @@
+select
+
+  body
+
+from raw.moby_dick
---
+++ models/word_count.sql
@@ -0,0 +1,23 @@
+{{config(materialized='table')}}
+
+with
+  moby_dick as (select * from {{ref('moby_dick')}})
+, words as (
+  select
+
+    word.value::string as word,
+    count(*) as ct
+
+  from moby_dick,
+
+  lateral flatten (input => split(moby_dick.body, ' ')) word
+
+  group by 1
+
+  order by 2 desc
+)
+
+select
+  *
+
+from words
