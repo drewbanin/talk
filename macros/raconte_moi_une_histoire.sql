b10d38bfa9041abb8f9607afa928d029a1449c6c
Drew Banin <drew@fishtownanalytics.com>
2018-12-14 11:35:04 -0500

    slower

---
+++ macros/raconte_moi_une_histoire.sql
@@ -0,0 +1,41 @@
+
+{% macro get_lines(limit) %}
+
+    -- call me ishmael
+    {% call statement("get_lines", fetch_result=true) %}
+
+        select body as line
+        from {{ ref('moby_dick') }}
+        limit {{ limit }}
+
+    {% endcall %}
+
+    {% set lines = load_result('get_lines').table.columns['line'].values() -%}
+    {{ return(lines) }}
+
+{% endmacro %}
+
+{% macro log_it(line, delay=1) %}
+    {{ log(line, info=True) }}
+    {% call statement() %}
+        select 1 as id from pg_sleep({{ delay }})
+    {% endcall %}
+{% endmacro %}
+
+
+{% macro raconte_moi_une_histoire() %}
+
+    -- {{ ref('moby_dick') }}
+
+    {% if not execute %}
+        {{ return([]) }}
+    {% endif %}
+
+    {% set lines = get_lines(limit=var('limit', 100)) %}
+
+    {%- for line_chunk in lines|batch(10) %}
+        {% set text = line_chunk | join("\n") %}
+        {{ log_it(text) }}
+    {% endfor %}
+
+{% endmacro %}
---
+++ models/slowby_dick.sql
@@ -0,0 +1,6 @@
+
+
+
+{% set _ = raconte_moi_une_histoire() %}
+
+select 1 as id
